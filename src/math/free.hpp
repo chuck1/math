07789a3b1dd78d374f932a093cd4f4eae6d7ce93
charles <charlesrymal@gmail.com>
2014-04-18 23:20:32 -0700

    cmake config fixing

--- src/math/free.hpp
+++ src/math/free.hpp
@@ -1,9 +1,7 @@
 #ifndef __MATH_FREE_H__
 #define __MATH_FREE_H__
 
-#ifdef TINYXML
 #include <tinyxml2.h>
-#endif
 
 #include <math/quat.hpp>
 #include <math/vec4.hpp>
@@ -11,15 +9,13 @@
 #include <math/color.hpp>
 
 namespace math {
-#ifdef TINYXML
 	float           	xml_parse_float(tinyxml2::XMLElement*, float = 0.0);
 	int			xml_parse_int(tinyxml2::XMLElement*, int);
 	math::quat      	xml_parse_quat(tinyxml2::XMLElement*, math::quat);
-	math::vec3      	xml_parse_vec3(tinyxml2::XMLElement*, math::vec3);
+	math::vec3<double>     	xml_parse_vec3(tinyxml2::XMLElement*, math::vec3<double>);
 	math::transform		xml_parse_transform(tinyxml2::XMLElement*, math::transform = math::transform());
 	math::color     	xml_parse_color(tinyxml2::XMLElement* element, math::color = math::black);
 	math::vec4      	xml_parse_vec4(tinyxml2::XMLElement* element);
-#endif
 
 	quat			slerp(quat, quat, float);
 
