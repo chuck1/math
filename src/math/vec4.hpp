d5ffeb7a06086f30707aba6e9e3ee0dff86980a3
Charles Rymal <charlesrymal@gmail.com>
2014-04-23 17:01:07 -0700

    templatizing vec4

--- src/math/vec4.hpp
+++ src/math/vec4.hpp
@@ -19,21 +19,131 @@
 #include <math/config.hpp>
 
 namespace math {
-	template <typename T> class vec4 {
+	template <typename T> class vec4: public vecbase<T,4> {
 		public:
-			//constructors
+			/** @name Constructors
+			 * @{
+			 */
+			vec4(const vec3<double> & rhs): x(rhs.v[0]), y(rhs.v[1]), z(rhs.v[2]), w(1.0f) {}
+			vec4(const vec3<double> & rhs,double const & newW): x(rhs.v[0]), y(rhs.v[1]), z(rhs.v[2]), w(newW) {}
 			vec4(): x(0.0f), y(0.0f), z(0.0f), w(0.0f) {}
-
-			vec4(double newX, double newY, double newZ, double newW)	
-				:	x(newX), y(newY), z(newZ), w(newW)
-			{}
-
-			vec4(const double * rhs)	:	x(*rhs), y(*(rhs+1)), z(*(rhs+2)), w(*(rhs+3)) {}
-
+			vec4(double newX, double newY, double newZ, double newW): x(newX), y(newY), z(newZ), w(newW) {}
+			vec4(const double * rhs): x(*rhs), y(*(rhs+1)), z(*(rhs+2)), w(*(rhs+3)) {}
 			vec4(const vec4 & rhs):	x(rhs.x), y(rhs.y), z(rhs.z), w(rhs.w) {}
+			/** @} */
+
+
+			bool	isNan() const {
+				if(isnan(x)) return true;
+				if(isnan(y)) return true;
+				if(isnan(z)) return true;
+				if(isnan(w)) return true;
+				return false;
+			}
+			bool	isFinite() const {
+				if(isinf(w)) return false;
+				if(isinf(x)) return false;
+				if(isinf(y)) return false;
+				if(isinf(z)) return false;
+				return true;
+			}
+
+			/** @name Rotations
+			 * @{
+			 */
+			void math::vec4::RotateX(double angle)
+			{
+				(*this)=GetRotatedX(angle);
+			}
+
+			math::vec4 math::vec4::GetRotatedX(double angle) const
+			{
+				math::vec3<double> v3d(x, y, z);
+
+				v3d.RotateX(angle);
+
+				return math::vec4(v3d, w);
+			}
+
+			void math::vec4::RotateY(double angle)
+			{
+				(*this)=GetRotatedY(angle);
+			}
+
+			math::vec4 math::vec4::GetRotatedY(double angle) const
+			{
+				math::vec3<double> v3d(x, y, z);
+
+				v3d.RotateY(angle);
+
+				return math::vec4(v3d, w);
+			}
+
+			void math::vec4::RotateZ(double angle)
+			{
+				(*this)=GetRotatedZ(angle);
+			}
+
+			math::vec4 math::vec4::GetRotatedZ(double angle) const
+			{
+				math::vec3<double> v3d(x, y, z);
+
+				v3d.RotateZ(angle);
+
+				return math::vec4(v3d, w);
+			}
+
+			void math::vec4::RotateAxis(double angle, const math::vec3<double> & axis)
+			{
+				(*this)=GetRotatedAxis(angle, axis);
+			}
+
+			math::vec4 math::vec4::GetRotatedAxis(double angle, const math::vec3<double> & axis) const
+			{
+				math::vec3<double> v3d(x, y, z);
+
+				v3d.RotateAxis(angle, axis);
+
+				return math::vec4(v3d, w);
+			}
+			/** @} */
+
+			/** @name Logical operators
+			 * @{
+			 */
+			bool math::vec4::operator==(const math::vec4 & rhs) const
+			{
+				if(x==rhs.x && y==rhs.y && z==rhs.z && w==rhs.w)
+					return true;
+
+				return false;
+			}
+			/** @} */
+
+			math::vec4::operator math::vec3<double>()
+			{
+				if(w==0.0f || w==1.0f)
+					return math::vec3<double>(x, y, z);
+				else
+					return math::vec3<double>(x/w, y/w, z/w);
+			}
+			math::mat44 math::vec4::operator*(math::vec4 const & rhs)
+			{
+				math::mat44 ret(
+						x*rhs.x, y*rhs.x, z*rhs.x, w*rhs.x,
+						x*rhs.y, y*rhs.y, z*rhs.y, w*rhs.y,
+						x*rhs.z, y*rhs.z, z*rhs.z, w*rhs.z,
+						x*rhs.w, y*rhs.w, z*rhs.w, w*rhs.w);
+
+				return ret;
+			}
+			void	math::vec4::print()
+			{
+				printf("%f %f %f %f\n",x,y,z,w);
+			}
+
+			//constructors
 			//convert v3d to v4d
-			vec4(const vec3<double> & rhs);
-			vec4(const vec3<double> & rhs,double const & newW);
 
 
 			~vec4() {}	//empty
@@ -134,12 +244,6 @@ namespace math {
 			operator const double* () const {return (const double*) this;}
 
 			operator vec3<double>();							//convert v4d to v3d
-
-			//member variables
-			double x;
-			double y;
-			double z;
-			double w;
 	};
 }
 
